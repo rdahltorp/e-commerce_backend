597ac2780dff877772b8c73f06ac23ec598d9356
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-11-12 08:47:28 -0700

    Added relationship code to index.

--- models/index.js
+++ models/index.js
@@ -15,10 +15,24 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE'
 })
 
-// Products belongToMany Tags (through ProductTag)
+//My thinking for the next could statements below, products will have may tags, so it will relate to the tags via the producttag. 
+//Then tags can be applied to multiple products so it will relate to the products thorugh the producttag
 
+// Products belongToMany Tags (through ProductTag) //Not sure this is right. Need to come back to it possibly. 
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+})
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag) //Not sure this is right. Need to come back to it possibly.
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+})
 
 module.exports = {
   Product,
